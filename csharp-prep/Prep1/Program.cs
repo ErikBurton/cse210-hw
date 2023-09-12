d7b77d81e3d1126105ef5799fe283b320c5dc301
Erik Burton <erikburton1@gmail.com>
2023-09-12 19:28:18 -0400

    C# Prep 1 - Complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) 
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Use Console.Write() to print a prompt.
+        Console.Write("What is your first name? ");
+        // Store the first name in a variable.
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        // Store the last name in a variable.
+        string lastName = Console.ReadLine();
+
+        // Print the full name using string interpoloaton ($"")
+        // Print the last name first and then the first name and again the last name for the desired format.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}. ");
     }
 }
