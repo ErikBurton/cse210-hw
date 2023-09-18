4d9134202fb300c98c1099f6b90e0365756a58af
Erik Burton <erikburton1@gmail.com>
2023-09-18 19:07:17 -0400

    Initial commit and adds step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+    
+
+            // Step 1 - ask user for magic number
+            Console.Write("Enter the magic number: ");
+            int magicNumber int.Parse(Console.ReadLine());
+
+            int guess;
+
+            // Step 2 - loop until guess is correct
+            do {
+                Console.Write("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+
+            if(guess < magicNumber) {
+                Console.Write("Higher!");
+            } else if (guess > magicNumber) {
+                Console.Write("Lower!");
+            } else {
+                Console.Write("You guessed it!");
+            }
+            } while (guess != magicNumber);
+
+
+        }
     }
+}
 }
