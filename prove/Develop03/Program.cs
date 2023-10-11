9aa8b3af66ec47f9a32cb0a058cbaa9471d465da
Erik Burton <erikburton1@gmail.com>
2023-10-11 18:24:40 -0400

    W05: Scripture Memorizer inital commit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,3 +7,8 @@ class Program
         Console.WriteLine("Hello Develop03 World!");
     }
 }
+
+// What does the program do? - Helps someone memorize scripture by hiding parts of the scripture so it can be memorized.
+// What user inputs does it have? - None: Enter or quit
+// What output does it produce? - Whole scripture and hides words when the user hits Enter.
+// How does the program end? - The program can end either by the user typing "quit" or when all of the words in the scripture have been hidden.
