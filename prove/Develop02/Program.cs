5cebb87e12afbc2d0c1c3ca7d5789e1149b8807f
Erik Burton <erikburton1@gmail.com>
2023-10-16 13:24:20 -0400

    Modifies the Program class and adds a loop

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,18 +5,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Create a new instance of the Journal class to manage journal entries
        Journal journal = new Journal();
-       PromptGenerator prompts = new PromptGenerator();
 
-       // Get random prompts and response
-       string prompts = prompts.GetRandomPrompt();
-       string response = GetUserResponse();
-
-       // Create entry
-       Entry entry = new Entry(prompts, response, DateTime.Now);
-
-       // Add entry to journal
-       journal.AddEntry(entry);
-     
+       // Loop to display the menu and handle user interaction
+       while (true)
+       {
+            // Display the menu options
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the Journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Exit");
+       }
+       
     }
 }
