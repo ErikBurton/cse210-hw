95a04ca5d727420ab4b6d8c997b039698386e788
Erik Burton <erikburton1@gmail.com>
2023-10-16 12:36:34 -0400

    Modifies the classes and implements Entry Class

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,8 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 public class Entry
 {
-    private string _prompt;
-    private string _response;
-    string DateTime date;
+    // 3 properties for Entry class
+    private string Prompt { get; }
+    private string Response { get; }
+    public string Date { get; }
+
+    // Constructor to create a new jounal entry
+    private Entry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+// ToString method provides formatted string repsenstation of the entry
+    private override string ToString()
+    {
+        return $"{Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+    }
 }
