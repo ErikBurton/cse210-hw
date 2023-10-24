2ceaa04eafc3a44ddd4be8679860fd016e10e4c6
Erik Burton <erikburton1@gmail.com>
2023-10-24 19:38:00 -0400

    Creates instances and sets values

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,12 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a new Assignment instance
-        Assignment assignment = new Assignment("Erik Burton", "Drum Set Performance");
+        // Create a new mathAssignment instance
+        MathAssignment mathAssignment = new MathAssignment("Erik Burton", "Drum Set Performance", "Independence", "Paradiddles");
 
-        // Call the GetSummary method and display the summar
-        string summary = assignment.GetSummary();
+        // Call the GetSummary method from the base class
+        string summary = mathAssignment.GetSummary();
+        Console.WriteLine("Assignment Summary");
         Console.WriteLine(summary);
+
+        // Call the GetHomeworkList method from the derived list
+        string homeworkList = mathAssignment.GetHomeWorkList();
+        Console.WriteLine("\nHomework List:");
+        Console.WriteLine(homeworkList);
 
     }
 }
