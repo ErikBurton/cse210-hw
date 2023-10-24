ef15c8dbc17ac60160e180c9346b724c82a5a73b
Erik Burton <erikburton1@gmail.com>
2023-10-24 18:37:55 -0400

    Initial commit - creates base class Assignment

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Assignment
+{
+    //  Private member variables
+    private string studentName;
+    private string topic;
+
+    // Constructor to initialize member variables
+    public Assignment(string studentName, string topic)
+    {
+        this.studentName = studentName;
+        this.topic = topic;
+    }
+
+    // Method to get summary
+    public string GetSummary()
+    {
+        return $"Student Name: {studentName}, Topic: {topic}";
+    }
+}
