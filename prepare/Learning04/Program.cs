474340527e3d74a34d12836a00ef47de6f704861
Erik Burton <erikburton1@gmail.com>
2023-10-24 18:43:16 -0400

    Create assignment calls method and displays

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a new Assignment instance
+        Assignment assignment = new Assignment("Erik Burton", "Drum Set Performance");
+
+        // Call the GetSummary method and display the summar
+        string summary = assignment.GetSummary();
+        Console.WriteLine(summary);
+
     }
 }
