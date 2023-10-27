876e15e45390b7bac89b5ccc293b10b4aaf2fc86
Erik Burton <erikburton1@gmail.com>
2023-10-27 17:28:42 -0400

    Builds out Word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,6 +1,26 @@
-using System;
+// Word class represents one word in the scripture
 
-public class ScriptureWord
+using System.ComponentModel.DataAnnotations;
+
+class Word 
 {
-    
+    private string text; // Make attribute private to encapsulate it
+
+    private bool hidden;
+
+    public Word(string text)
+    {
+        this.text = text;
+        hidden = false;
+    }
+
+    public void Hide()
+    {
+        hidden = true;
+    }
+
+    public string GetText()
+    {
+        return text; // Getter method to access the private attribute
+    }
 }
