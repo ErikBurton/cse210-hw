e35f1fa2464ceb364650d4ec2f47faaa0479f30f
Erik Burton <erikburton1@gmail.com>
2023-11-04 14:46:17 -0400

    Start base class MindfulnessActivity

--- prove/Develop04/MindfulnessActivity.cs
+++ prove/Develop04/MindfulnessActivity.cs
@@ -2,5 +2,14 @@ using System;
 
 class MindfulnessActivity
 {
-    
+    protected string name;
+
+    protected string description;
+
+    public MindfulnessActivity(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
 }
