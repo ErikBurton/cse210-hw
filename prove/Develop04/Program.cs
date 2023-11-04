29c7ec9c38bfad73c81e16729577480bb3c224ea
Erik Burton <erikburton1@gmail.com>
2023-11-04 14:12:42 -0400

    First commit. Creates classes

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,16 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create journal
-        Journal journal = new Journal();
-
-        // Promts
-        string[] prompts = {
-            "What did you get done today?",
-            "What made you smile today?",
-            "Who did you talk to today?",
-            "What is your Frog to eat tomorrow?",
-            "What are you greatful for?"
-        };
+     
     }
 }
