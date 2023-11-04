881a61074a15a115956f727a1d023e7f1d5478dd
Erik Burton <erikburton1@gmail.com>
2023-11-04 14:57:12 -0400

    Adds methods to Start and End acvitivty

--- prove/Develop04/MindfulnessActivity.cs
+++ prove/Develop04/MindfulnessActivity.cs
@@ -1,15 +1,35 @@
 using System;
+using System.Dynamic;
+using System.Reflection.Emit;
 
 class MindfulnessActivity
 {
     protected string name;
-
     protected string description;
 
+    // ConstructorBuilder to initialize name and description
     public MindfulnessActivity(string name, string description)
     {
         this.name = name;
         this.description = description;
     }
+
+    // Start method for mindfulness activity
+    public virtual void Start()
+    {
+        SetDuration();
+        ShowStartingMessage();
+        PrepareToBegin();
+
+    }
+
+    // End method for mindfulness activity
+    public virtual void End()
+    {
+        ShowActivityComnpletion();
+        CongratulateUser()
+    }
+
+
 
 }
