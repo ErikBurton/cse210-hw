4f7cb0d03051c73e93f3249b86a4d9e525101700
Erik Burton <erikburton1@gmail.com>
2023-11-07 18:52:50 -0500

    Builds out and completes the Base class

--- prepare/Learning05/Shape.cs
+++ prepare/Learning05/Shape.cs
@@ -1,4 +1,20 @@
+// Class to represent shapes
 public class Shape
 {
-    
+    private string _color; // Private member to store the color of the shape
+
+    public Shape(string color) // Constructor to initialze the color
+    {
+        _color = color; 
+    }
+
+    public string GetColor() // Getter to retrieve the color
+    {
+        return _color;
+    }
+
+    public virtual double GetArea() // Polymorphism - Virtual method to calculate the area
+    {
+        return 0; // Bass class returns 0 
+    }  
 }
