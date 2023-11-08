a60de634946a88ac1ebe279aee59f2cd9d2b2a9a
Erik Burton <erikburton1@gmail.com>
2023-11-07 19:16:02 -0500

    Fixes typo

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -4,7 +4,7 @@ public class Circle : Shape // Inherits from the base class
 {
     private double _radius; // Private variable to store radius
 
-    private Circle(string color, double _radius) // Constructor to initialize color and radius
+    private Circle(string color, double radius) // Constructor to initialize color and radius
         : base(color)
     {
         _radius = radius;
@@ -12,6 +12,6 @@ public class Circle : Shape // Inherits from the base class
 
     public override double GetArea()
     {
-        return Math.PI * _radius * _radius
+        return Math.PI * _radius * _radius;
     }
 }
