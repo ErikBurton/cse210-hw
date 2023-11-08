415b6dd35e45c385f5f3e99c55b2aec5d3bdf9ca
Erik Burton <erikburton1@gmail.com>
2023-11-07 19:28:31 -0500

    Builds out main program class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new list<Shape>(); // Creates and empty list
+
+        // Create instances of Square, Rectangle and Circle and adds them to the list
+        shapes.Add(new Square("Yellow", 5.0));
+        shapes.Add(new Rectangle("Black", 4.0, 6.0));
+        shapes.Add(new Circle("Orange", 3.0));
     }
 }
