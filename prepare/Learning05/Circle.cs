d7efa08b102da1903638a1409010ac475453ab9d
Erik Burton <erikburton1@gmail.com>
2023-11-07 19:23:19 -0500

    Adds comments

--- prepare/Learning05/Circle.cs
+++ prepare/Learning05/Circle.cs
@@ -10,8 +10,8 @@ public class Circle : Shape // Inherits from the base class
         _radius = radius;
     }
 
-    public override double GetArea()
+    public override double GetArea() // Polymorphism: Override the GetArea for circles to calculate their area
     {
-        return Math.PI * _radius * _radius;
+        return Math.PI * _radius * _radius; // Calculate the area of the circle
     }
 }
