d8f84758cbe8c02a1921cd627c6b11ae15a71077
Erik Burton <erikburton1@gmail.com>
2023-11-07 19:21:43 -0500

    Builds out Square class

--- prepare/Learning05/Square.cs
+++ prepare/Learning05/Square.cs
@@ -1,4 +1,16 @@
-public class Square
+// Class for Square
+public class Square : Shape
 {
-    
+    private double _side; // Private variable to store side of a square
+
+    public Square(string color, double side) // Constructor to initialize color and side of the square
+        : base(color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea() // Polymorphism: Override the GetArea method for squares to calculate their area
+    {
+        return _side * _side; // Calculate the area of a square
+    }
 }
