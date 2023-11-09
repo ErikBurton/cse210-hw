ee188f30efe97ef8c81c388f733ac91333bc7fd4
Erik Burton <erikburton1@gmail.com>
2023-11-09 18:37:48 -0500

    Creates array for prompts

--- prove/Develop04/ListeningActivity.cs
+++ prove/Develop04/ListeningActivity.cs
@@ -2,5 +2,11 @@ using System;
 
 class ListeningActivity
 {
-    
+     private string[] prompts = {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
 }
