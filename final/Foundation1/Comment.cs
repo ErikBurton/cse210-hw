6e069b22b35c957624b37b3e6bca2cd9fc740331
Erik Burton <erikburton1@gmail.com>
2023-12-01 10:36:37 -0500

    Renames Comment - adds comment class

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+// Comment class to store information about a comment
+class Comment{
+    public string CommenterName { get; set; }
+    public string CommentText { get; set; }
+}
+
