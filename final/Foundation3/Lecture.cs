d3bc2ba653f91feeb3971aadc36b491e61f6be75
Erik Burton <erikburton1@gmail.com>
2023-12-04 18:57:49 -0500

    Builds out - defines derived Lecture class

--- final/Foundation3/Lecture.cs
+++ final/Foundation3/Lecture.cs
@@ -1,5 +1,20 @@
-// Lecture class
-class Lecture
+// Derived Lecture class
+class Lecture : Event
 {
-    
+    private string _speaker;
+    private int _capacity;
+
+    // Constructor to initialize lecture-specific properties
+    public Lecture(string _title, string _description, DateTime _date, TimeSpan _time, Address _address, string _speaker, int _capacity)
+        : base(_title, _description, _date, _time, _address)
+    {
+        this._speaker = _speaker;
+        this._capacity = _capacity;
+    }
+
+    // Override method for generating full details, including lecture-specific information
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}, Type: Lecture, Speaker: {_speaker}, Capacity: {_capacity}";
+    }
 }
