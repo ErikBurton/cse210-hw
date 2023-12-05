d66f8276c63ea3e45a1cff6089265896c395db8e
Erik Burton <erikburton1@gmail.com>
2023-12-04 20:06:37 -0500

    Builds out derived OutdoorGathering class

--- final/Foundation3/OutdoorGathering.cs
+++ final/Foundation3/OutdoorGathering.cs
@@ -1,5 +1,18 @@
-// OutdoorGathering class
-class OutdoorGathering
+// Derived OutdoorGathering class
+class OutdoorGathering : Event
 {
-    
+    private string _weatherStatement;
+
+    // Constructor to initialize outdoor gathering-specific properties
+    public OutdoorGathering(string _title, string _description, DateTime _date, TimeSpan _time, Address _address, string _weatherStatement)
+        : base(_title, _description, _date, _time, _address)
+    {
+        this._weatherStatement = _weatherStatement;
+    }
+
+    // Override method for generating full details, including outdoor gathering-specific information
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}, Type: Outdoor Gathering, Weather: {_weatherStatement}";
+    }
 }
