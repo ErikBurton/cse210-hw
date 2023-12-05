24998a00646dad1eddb35d1e3713ab517eb6f045
Erik Burton <erikburton1@gmail.com>
2023-12-04 20:03:34 -0500

    Builds out derived Reciption class

--- final/Foundation3/Reception.cs
+++ final/Foundation3/Reception.cs
@@ -1,5 +1,18 @@
-// Reception class
-class Reception
+// Derived Reception class
+class Reception : Event
 {
-    
+    private string _rsvpEmail;
+
+    // Constructor with underscore prefix to initialize reception-specific properties
+    public Reception(string _title, string _description, DateTime _date, TimeSpan _time, Address _address, string _rsvpEmail)
+        : base(_title, _description, _date, _time, _address)
+    {
+        this._rsvpEmail = _rsvpEmail;
+    }
+
+    // Override method for generating full details, including reception-specific information
+    public override string GenerateFullDetails()
+    {
+        return $"{base.GenerateFullDetails()}, Type: Reception, RSVP Email: {_rsvpEmail}";
+    }
 }
